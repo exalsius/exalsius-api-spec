193ea7c4535224b52f182c938cd4262f2bee7169
srnbckr <soeren.becker@logsight.ai>
2025-05-16 18:50:13 +0200

    fix: use API version as python packageVersion

--- scripts/generate-client.sh
+++ scripts/generate-client.sh
@@ -5,6 +5,7 @@ set -euo pipefail
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 REPO_DIR="$( cd "${SCRIPT_DIR}/.." && pwd )"
 CLIENT_SDK_DIR="client-sdk"
+API_VERSION=$(grep -oP 'version: \K[^"]+' ${REPO_DIR}/openapi/openapi.yaml)
 
 log() {
     echo "[$(date +'%Y-%m-%d %H:%M:%S')] $1"
@@ -27,19 +28,19 @@ create_bundle() {
 		redocly/cli \
 		--config /spec/openapi/redocly.yaml \
 		bundle /spec/openapi/openapi.yaml -o /spec/dist/bundle.yaml
-
 }
 
 
 run_generator() {
     log "Generating client SDK python code..."
     docker run --rm -v "${REPO_DIR}:/local" \
-            --user $(id -u):$(id -g) \
-            openapitools/openapi-generator-cli generate \
-            -i local/dist/bundle.yaml \
-            --package-name "exalsius_api_client" \
-            -g python \
-            -o /local/${CLIENT_SDK_DIR}
+        --user $(id -u):$(id -g) \
+        openapitools/openapi-generator-cli generate \
+        -i local/dist/bundle.yaml \
+        --package-name "exalsius_api_client" \
+        -g python \
+        --additional-properties=packageVersion=${API_VERSION} \
+        -o /local/${CLIENT_SDK_DIR}
 }
 
 run_formatter() {
